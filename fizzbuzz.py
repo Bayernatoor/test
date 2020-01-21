5ada5d1e5b48790ab35c9cce106b1572692c34c0
Bayer <Bayernator@protonmail.com>
2020-01-21 13:13:48 -0600

    enhanced fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,7 @@
 Fizzbuzz = []
+num_3 = []
+num_5 = []
+num_none = []
 
 for num in range(1, 101):
     if num % 5 == 0 and num % 3 == 0:
@@ -6,9 +9,16 @@ for num in range(1, 101):
         Fizzbuzz.append(num)
     elif num % 3 == 0:
         print("Fizz: {}".format(num))
+        num_3.append(num)
     elif num % 5 == 0:
         print("Buzz: {}".format(num))
+        num_5.append(num)
     else:
         print("Num: {}".format(num))
+        num_none.append(num)
 
-print(Fizzbuzz)
+
+print(Fizzbuzz , Fizzbuzz)
+print('3: ', num_3)
+print('5: ', num_5)
+print('none: ', num_none)
